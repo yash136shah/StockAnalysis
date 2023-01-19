32a52da43f0524ec94f1c13807c4c1e8a5addc39
Yash Shah <105405680+yash136shah@users.noreply.github.com>
2023-01-19 11:37:01 -0800

    Update stockAnalysis-2.py

--- stockAnalysis-2.py
+++ stockAnalysis-2.py
@@ -390,7 +390,7 @@ if st.session_state["name_search"]== 'Sector & Industry':
    
         fig = px.scatter(isdfn,x=x_axis_met,y=y_axis_met,color=marker_color,size=marker_size,size_max=40,text=coName)
         
-        containerNameChart.write(st.session_state["nameSel"])
+        containerNameChart.write(st.session_state["name_selected_SI"])
         
         try:
             issi = multidfC[multidfC[coName].isin(st.session_state["nameSel"])]
