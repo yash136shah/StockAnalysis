77a57a7078e2b0b01ffacd03f380e4de555bfd9e
Yash Shah <105405680+yash136shah@users.noreply.github.com>
2023-01-19 11:50:15 -0800

    Update stockAnalysis-2.py

--- stockAnalysis-2.py
+++ stockAnalysis-2.py
@@ -388,17 +388,17 @@ if st.session_state["name_search"]== 'Sector & Industry':
             marker_color = st.selectbox("Marker Color",multidfCStr.columns,index=indusindex,key="markercolor")
 
    
-        fig = px.scatter(isdfn,x=x_axis_met,y=y_axis_met,color=marker_color,size=marker_size,size_max=40,text=coName)
+        fig = px.scatter(isdfn,x=x_axis_met,y=y_axis_met,color=marker_color,size=marker_size,size_max=40,text=coName,width=1000, height=600)
         
         
-        try:
-            issi = multidfC[multidfC[coName].isin(st.session_state["name_selected_SI"])]
-            xh = issi[x_axis_met].tolist()
-            yh= issi[y_axis_met].tolist()
-            fig.add_trace(go.Scatter(x=xh, y=yh, mode = 'markers',marker_symbol = 'star',marker_size = 60,opacity=0.5,fillcolor="orange",name="Selected Companies"))
-            st.write("working!")
-        except:
-            pass
+        #try:
+         #   issi = multidfC[multidfC[coName].isin(st.session_state["name_selected_SI"])]
+         #   xh = issi[x_axis_met].tolist()
+         #   yh= issi[y_axis_met].tolist()
+         #   fig.add_trace(go.Scatter(x=xh, y=yh, mode = 'markers',marker_symbol = 'star',marker_size = 60,opacity=0.5,fillcolor="orange",name="Selected Companies"))
+         #   st.write("working!")
+        #except:
+         #   pass
         
         col1,col2,col3,col4 = st.columns([1,1,4,4])
         x_min = isdfn[x_axis_met].min()
