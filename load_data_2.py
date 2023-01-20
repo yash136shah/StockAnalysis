4add9d394dfdc9a6b24d337240a74eaa5de0dcce
Yash Shah <105405680+yash136shah@users.noreply.github.com>
2023-01-19 17:52:14 -0800

    Update load_data_2.py

--- load_data_2.py
+++ load_data_2.py
@@ -11,12 +11,6 @@ from numerize import numerize as nu
 # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
 fs = s3fs.S3FileSystem(anon=False)
 
-# Retrieve file contents.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-
-def read_file(filename):
-        
-
 
 # VARIABLE INITIALIZED 
 sector = 'SECTOR'
