fd40c62b62dd6fd9b88665b266c586a046254783
Yash Shah <105405680+yash136shah@users.noreply.github.com>
2023-01-20 14:53:34 -0800

    Update 2_Industry_Overview.py

--- pages/2_Industry_Overview.py
+++ pages/2_Industry_Overview.py
@@ -57,10 +57,6 @@ if "marketSelect" not in st.session_state:
     st.session_state["marketSelect"] = markets[2]
     st.session_state["marketIndex"] = markets.index(st.session_state["marketSelect"])
 
-def MarketSelect ():
-    st.session_state["marketIndex"] = markets.index(st.session_state["marketSelrad"])
-
-st.session_state["marketSelect"] = st.sidebar.radio("Market:",markets,index=st.session_state["marketIndex"],key="marketSelrad",on_change=MarketSelect)
 
 #MARKET SELECT 
 
